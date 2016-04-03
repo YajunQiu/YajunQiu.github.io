6c7280967c6342eb3df7f4e5d6be60b2939ba1ae
YajunQiu <yajun.qiu@utexas.edu>
2016-04-03 13:07:56 -0500

    Launch Personal Website 1st Version
    Add all files of Yajun Qiu's 1st version, including pages and videos

---
+++ quiz/quiz_ui.js
@@ -0,0 +1,46 @@
+var QuizUI = {
+    displayNext: function () {
+        if (quiz.hasEnded()) {
+            this.displayScore();
+        } else {
+            this.displayQuestion();
+            this.displayChoices();
+            this.displayProgress();
+        }
+    },
+    displayQuestion: function() {
+        this.populateIdWithHTML("question", quiz.getCurrentQuestion().text);
+    },
+    displayChoices: function() {
+        var choices = quiz.getCurrentQuestion().choices;
+
+        for(var i = 0; i < choices.length; i++) {
+            this.populateIdWithHTML("choice" + i, choices[i]);
+            this.guessHandler("guess" + i, choices[i]);
+        }
+    },
+    displayScore: function() {
+        var gameOverHTML = "<h1>测试结束，感谢您的参与。</h1>";
+        var currentQuestionNumber = quiz.currentQuestionIndex;
+        var testResult = Math.round(quiz.score/currentQuestionNumber*100)
+        gameOverHTML += "<h2> 您测试结果: " + testResult + "/100" + "</h2>";
+        this.populateIdWithHTML("quiz", gameOverHTML);
+    },
+    
+    populateIdWithHTML: function(id, text) {
+        var element = document.getElementById(id);
+        element.innerHTML = text;
+    },
+    guessHandler: function(id, guess) {
+        var button = document.getElementById(id);
+        button.onclick = function() {
+            quiz.guess(guess);
+            QuizUI.displayNext();
+        }
+    },
+    
+    displayProgress: function() {
+        var currentQuestionNumber = quiz.currentQuestionIndex + 1;
+        this.populateIdWithHTML("progress", "第" + currentQuestionNumber + "题" + "  共" + quiz.questions.length + "题");
+    }
+};
